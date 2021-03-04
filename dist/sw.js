fb47b36d9e91eb182df73efea28772a50595a601
David Hillman <dhillman1989@gmail.com>
2021-03-04 20:32:54 +0000

    removed watch from build

--- dist/sw.js
+++ dist/sw.js
@@ -121,7 +121,7 @@ define("./sw.js",['./workbox-4b5c23d1'], function (workbox) { 'use strict';
     "revision": "213a6fb994faca9736132fa9348b8700"
   }, {
     "url": "main.js",
-    "revision": "eeacedd42d33254a9a1ce738198d98ee"
+    "revision": "e46afeddb093afffa8f1ef44775698f5"
   }], {});
 
 });
